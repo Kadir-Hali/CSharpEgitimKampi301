7fc5f92fb2c000c20b97c8137445dba5fda90c57
Kadir-Hali <kadirhali07@gmail.com>
2025-02-12 11:53:55 +0300

    12.02.25-1

--- CSharpEgitimKampi301.BusinessLayer/Concrete/OrderManager.cs
+++ CSharpEgitimKampi301.BusinessLayer/Concrete/OrderManager.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Abstract;
 using CSharpEgitimKampi301.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,13 @@ namespace CSharpEgitimKampi301.BusinessLayer.Concrete
 {
     public class OrderManager : IOrderService
     {
+        private readonly IOrderDal _orderDal;
+
+        public OrderManager(IOrderDal orderDal)
+        {
+            _orderDal = orderDal;
+        }
+
         public void TDelete(Order entity)
         {
             throw new NotImplementedException();
